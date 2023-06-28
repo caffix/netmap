51f1d9841fca0cc635e9f04e6ea1c6a31d4e8ce7
caffix <caffix@users.noreply.github.com>
2023-06-28 14:41:54 -0400

    added a Remove method for a Graph

--- graph.go
+++ graph.go
@@ -8,10 +8,11 @@ import (
 	"embed"
 	"fmt"
 	"math/rand"
+	"os"
 
 	db "github.com/owasp-amass/asset-db"
-	"github.com/owasp-amass/asset-db/migrations/postgres"
-	"github.com/owasp-amass/asset-db/migrations/sqlite3"
+	pgmigrations "github.com/owasp-amass/asset-db/migrations/postgres"
+	sqlitemigrations "github.com/owasp-amass/asset-db/migrations/sqlite3"
 	"github.com/owasp-amass/asset-db/repository"
 	migrate "github.com/rubenv/sql-migrate"
 	"gorm.io/driver/postgres"
@@ -21,8 +22,9 @@ import (
 
 // Graph is the object for managing a network infrastructure link graph.
 type Graph struct {
-	DB  *db.AssetDB
-	dsn string
+	DB     *db.AssetDB
+	dsn    string
+	dbtype repository.DBType
 }
 
 // NewGraph returns an intialized Graph object.
@@ -35,16 +37,14 @@ func NewGraph(system, path string, options string) *Graph {
 	var name string
 	var fs embed.FS
 	var database gorm.Dialector
-	switch system {
-	case "memory":
-		fallthrough
-	case "local":
+	switch g.dbtype {
+	case repository.SQLite:
 		name = "sqlite3"
-		fs = sqlite3.Migrations()
+		fs = sqlitemigrations.Migrations()
 		database = sqlite.Open(g.dsn)
-	case "postgres":
+	case repository.Postgres:
 		name = "postgres"
-		fs = postgres.Migrations()
+		fs = pgmigrations.Migrations()
 		database = postgres.Open(g.dsn)
 	}
 
@@ -90,7 +90,39 @@ func OpenGraph(system, path string, options string) *Graph {
 	}
 
 	return &Graph{
-		DB:  store,
-		dsn: dsn,
+		DB:     store,
+		dsn:    dsn,
+		dbtype: dbtype,
+	}
+}
+
+func (g *Graph) Remove() {
+	switch g.dbtype {
+	case repository.SQLite:
+		os.Remove(g.dsn)
+	case repository.Postgres:
+		teardownPostgres(g.dsn)
+	}
+}
+
+func teardownPostgres(dsn string) {
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+
+	migrationsSource := migrate.EmbedFileSystemMigrationSource{
+		FileSystem: pgmigrations.Migrations(),
+		Root:       "/",
+	}
+
+	sqlDb, err := db.DB()
+	if err != nil {
+		panic(err)
+	}
+
+	_, err = migrate.Exec(sqlDb, "postgres", migrationsSource, migrate.Down)
+	if err != nil {
+		panic(err)
 	}
 }
